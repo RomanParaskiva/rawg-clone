da32d61f1b90d34bb1f8a295386a484f1fcbd066
Roman Paraskiva <parasta2018@gmail.com>
2023-02-19 17:25:02 +0300

    fix next/image Un-configured Host

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
     // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
     styledComponents: true
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.rawg.io',
+        port: '',
+        pathname: '/media/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
